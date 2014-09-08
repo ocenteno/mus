cf7b8076e482f9ad221658bc39c70af5e5295082
Javier Ponce <javierpm@hotmail.com>
2014-09-08 17:31:34 +0200

    Subimos la clase Jugador terminada con su test de pruebas y la interface del gestor de la baraja.

--- dominio-cartas/src/main/java/es/insa/proyecto/dominio/cartas/Jugador.java
+++ dominio-cartas/src/main/java/es/insa/proyecto/dominio/cartas/Jugador.java
@@ -4,57 +4,65 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * Esta clase contiene los métodos que interactúan con el Jugador. Las
+ * operaciones que se pueden realizar son las siguientes:
  * 
- * @author insa15
- *
+ * - Añadir Carta a la mano del jugador. - Quitar una Carta de la mano del
+ * jugador. - Consultar la mano del jugador.
+ * 
+ * @author Cristina y Javier
+ * 
  */
 public class Jugador {
 
-	// Se debe sustituir Object por Carta una vez exista la clase
 	private String nombre;
-	private Set<Object> manoJugador;
-	
+	private Set<Carta> manoJugador;
+
 	public Jugador() {
 	}
 
-		public Jugador(String nombre) {
-			super();
-			this.nombre = nombre;
-			this.manoJugador = new HashSet<Object>();
-		}
-		
-		public Jugador(String nombre, Set<Object> manoJugador) {
-			super();
-			this.nombre = nombre;
-			this.manoJugador = manoJugador;
-		}
-		
-		// Se debe sustituir Object por Carta una vez exista la clase
-		public void añadirCarta(Object cartaAAñadir){
-			this.manoJugador.add(cartaAAñadir);
-		}
-	
-		// Se debe sustituir Object por Carta una vez exista la clase
-		public void quitarCarta(Object cartaAQuitar){
-			this.manoJugador.remove(cartaAQuitar);
-		}
-	
-		// Se debe sustituir Object por Carta una vez exista la clase
-		/**
-		 * La mano del jugador la guardamos como un Set pero la devolvemos como un Array de Cartas
-		 * @return devuelve un Array de Carta
-		 */
-		public Object[] getMano(){
-			return this.manoJugador.toArray(new Object[manoJugador.size()]);
-			
-		}
-	
-		public String getNombre() {
-			return nombre;
-		}
-	
-		public void setNombre(String nombre) {
-			this.nombre = nombre;
-		}
-	
+	public Jugador(String nombre) {
+		super();
+		this.nombre = nombre;
+		this.manoJugador = new HashSet<Carta>();
+	}
+
+	/**
+	 * Método para añadir una Carta a la mano del jugador.
+	 * 
+	 * @param cartaAAñadir
+	 *            --> la carta que se pretende añadir
+	 */
+	public void añadirCarta(Carta cartaAAñadir) {
+		this.manoJugador.add(cartaAAñadir);
+	}
+
+	/**
+	 * Método para quitar una Carta de la mano del jugador.
+	 * 
+	 * @param cartaAQuitar
+	 *            --> la carta que se pretende quitar
+	 */
+	public void quitarCarta(Carta cartaAQuitar) {
+		this.manoJugador.remove(cartaAQuitar);
+	}
+
+	/**
+	 * La mano del jugador la guardamos como un Set pero la devolvemos como un
+	 * Array de Cartas
+	 * 
+	 * @return devuelve un Array de Carta
+	 */
+	public Carta[] getMano() {
+		return this.manoJugador.toArray(new Carta[0]);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
 }
--- dominio-cartas/src/test/java/es/insa/proyecto/dominio/cartas/test/TestJugador.java
+++ dominio-cartas/src/test/java/es/insa/proyecto/dominio/cartas/test/TestJugador.java
@@ -1,17 +1,13 @@
 package es.insa.proyecto.dominio.cartas.test;
 
-import static org.junit.Assert.*;
-
-import java.util.HashSet;
-import java.util.Set;
-
 import org.junit.Assert;
 import org.junit.Test;
 
+import es.insa.proyecto.dominio.cartas.Carta;
 import es.insa.proyecto.dominio.cartas.Jugador;
+import es.insa.proyecto.dominio.cartas.Palo;
 
 public class TestJugador {
-	private String nombre;
 	
 	@Test
 	public void testCrearJugador() {
@@ -26,7 +22,7 @@ public class TestJugador {
 	@Test
 	public void testAñadirCarta() {
 		// 1º preparación
-		Object carta = new Object();
+		Carta carta = new Carta();
 		Jugador jugador = new Jugador("Jugador1");
 		// 2º ejecución
 		jugador.añadirCarta(carta);
@@ -37,7 +33,7 @@ public class TestJugador {
 	@Test
 	public void testQuitarCarta() {
 		// 1º preparación
-		Object carta = new Object();
+		Carta carta = new Carta();
 		Jugador jugador = new Jugador("Jugador1");
 		jugador.añadirCarta(carta);
 		// 2º ejecución
@@ -49,19 +45,15 @@ public class TestJugador {
 	@Test
 	public void testConsultarMano() {
 		// 1º preparación
-		Object carta1 = new Object();
-		Object carta2 = new Object();
-		Object carta3 = new Object();
-		Object carta4 = new Object();
 		Jugador jugador = new Jugador("Jugador1");
-		jugador.añadirCarta(carta1);
-		jugador.añadirCarta(carta2);
-		jugador.añadirCarta(carta3);
-		jugador.añadirCarta(carta4);
+		jugador.añadirCarta(new Carta(Palo.BASTOS, 10, 10));
+		jugador.añadirCarta(new Carta(Palo.BASTOS, 11, 10));
+		jugador.añadirCarta(new Carta(Palo.BASTOS, 12, 10));
+		jugador.añadirCarta(new Carta(Palo.BASTOS, 7, 7));
 		// 2º ejecución
-		jugador.getMano();
+		Carta[] mano = jugador.getMano();
 		// 3º aserción
-		Assert.assertEquals("Ej jugador debe tener una mano con 4 cartas", 4, jugador.getMano().length);
+		Assert.assertEquals("Ej jugador debe tener una mano con 4 cartas", 4, mano.length);
 	}
 
 	
