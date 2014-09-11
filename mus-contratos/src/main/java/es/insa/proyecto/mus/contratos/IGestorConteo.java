bf50d97faa847718e3265d542dbdf8e323cf7b1d
Cristina <cris.eizaguirre@gmail.com>
2014-09-11 09:28:52 +0200

    Guardar gestor de conteo

---
+++ mus-contratos/src/main/java/es/insa/proyecto/mus/contratos/IGestorConteo.java
@@ -0,0 +1,14 @@
+package es.insa.proyecto.mus.contratos;
+
+import es.insa.proyecto.dominio.cartas.Jugador;
+
+/**
+ * Este gestor se encargará de contar las piedras que otorga una jugada
+ * @author Jose Antonio y Cristina
+ *
+ */
+public interface IGestorConteo {
+	
+	public void contarLasPiedrasPorJugada();
+	
+}
