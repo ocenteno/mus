416b520dc2b9965fcf0376d3ca69992144898d2a
José Antonio García <jagc.temenos@gmail.com>
2014-09-19 12:29:12 +0200

    Gestor de tanteos

---
+++ mus-contratos/src/main/java/es/insa/proyecto/mus/contratos/IGestorTanteoParcial.java
@@ -0,0 +1,20 @@
+package es.insa.proyecto.mus.contratos;
+
+import es.insa.proyecto.dominio.cartas.Jugador;
+import es.insa.proyecto.mus.modelo.Lances;
+
+/**
+ * Interface que realiza la suma del tanteo parcial al bote general
+ * de cada pareja.
+ * @author Cristina y Javier
+ *
+ */
+public interface IGestorTanteoParcial {
+	
+	/**
+	 * Buscamos al jugador que ha ganado el lance
+	 */
+	public Jugador sacarPiedrasLance( Lances lance, Jugador... jugadores);
+	
+}
+
---
+++ mus-negocio/src/main/java/es/insa/proyecto/mus/negocio/GestorTanteoParcial.java
@@ -0,0 +1,64 @@
+package es.insa.proyecto.mus.negocio;
+
+import es.insa.proyecto.dominio.cartas.Jugador;
+import es.insa.proyecto.mus.contratos.IGestorDeApuestas;
+import es.insa.proyecto.mus.contratos.IGestorTanteoParcial;
+import es.insa.proyecto.mus.modelo.Lances;
+import es.insa.proyecto.mus.modelo.Pareja;
+import es.insa.proyecto.mus.modelo.Partida;
+
+public class GestorTanteoParcial implements IGestorTanteoParcial{
+	
+	private GanadorGrande ganadorGrande;
+	private GanadorChica ganadorChica;
+	private GanadorPares ganadorPares;
+	private GanadorJuego ganadorJuego;
+	private GanadorPunto ganadorPunto;
+	private Pareja pareja;
+	private Partida partida;
+	private IGestorDeApuestas gestorApuestas;
+	private Jugador jugador;
+
+	public GestorTanteoParcial() {
+	}
+
+	@Override
+	public Jugador sacarPiedrasLance(Lances lance, Jugador... jugadores) {
+		switch (lance) {
+		case GRANDE:
+			jugador = ganadorGrande.ganador(jugadores);
+			break;
+		case CHICA:
+			jugador = ganadorChica.ganador(jugadores);
+			break;
+		case PARES:
+			jugador = ganadorPares.ganador(jugadores);
+			break;
+		case JUEGO:
+			jugador = ganadorJuego.ganador(jugadores);
+			break;
+		case PUNTO:
+			jugador = ganadorPunto.ganador(jugadores);
+			break;
+		default:
+			break;
+	}
+		int bote = gestorApuestas.getApuestas(lance);
+		if (partida.getPareja1().comprobarPertenece(jugador)){
+			partida.getPareja1().setPiedrasGanadas(bote);
+		}else{
+			partida.getPareja2().setPiedrasGanadas(bote);
+		}
+		
+		return jugador;
+		
+	}
+
+
+	public void añadirBoteAPareja(int bote) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
