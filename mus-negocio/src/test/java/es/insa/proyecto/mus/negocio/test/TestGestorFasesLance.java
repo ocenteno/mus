df5c12f033c5ca379c5d8504d44e2f40d1e807b6
José Antonio García <jagc.temenos@gmail.com>
2014-09-19 10:07:28 +0200

    Realización de test del GestorFasesLance

--- mus-negocio/src/test/java/es/insa/proyecto/mus/negocio/test/TestGestorFasesLance.java
+++ mus-negocio/src/test/java/es/insa/proyecto/mus/negocio/test/TestGestorFasesLance.java
@@ -3,6 +3,7 @@ package es.insa.proyecto.mus.negocio.test;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -15,6 +16,7 @@ import es.insa.proyecto.dominio.cartas.Pito;
 import es.insa.proyecto.mus.modelo.Lances;
 import es.insa.proyecto.mus.modelo.Partida;
 import es.insa.proyecto.mus.negocio.ComprobadorParesJuego;
+import es.insa.proyecto.mus.negocio.GestorDeApuestas;
 import es.insa.proyecto.mus.negocio.GestorFasesLance;
 
 /**
@@ -29,10 +31,11 @@ public class TestGestorFasesLance {
 	private static Jugador j3;
 	private static Jugador j4;
 	private static GestorFasesLance miGestor;
+	private static GestorDeApuestas gestorApuestas;
+	private static Partida p;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		Partida p = new Partida();
 		j1 = new Jugador("Jugador1");
 		j2 = new Jugador("Jugador2");
 		j3 = new Jugador("Jugador3");
@@ -43,12 +46,12 @@ public class TestGestorFasesLance {
 		j1.añadirCarta(new Carta(Palo.BASTOS, 7, 7));
 		//
 		j2.añadirCarta(new Carta(Palo.ESPADAS, 10, 10));
-		j2.añadirCarta(new Carta(Palo.ESPADAS, 12, 10));
+		j2.añadirCarta(new Carta(Palo.ESPADAS, 11, 10));
 		j2.añadirCarta(new Carta(Palo.ESPADAS, 12, 10));
 		j2.añadirCarta(new Carta(Palo.ESPADAS, 7, 7));
 		//
 		j3.añadirCarta(new Carta(Palo.OROS, 10, 10));
-		j3.añadirCarta(new Carta(Palo.OROS, 11, 10));
+		j3.añadirCarta(new Carta(Palo.OROS, 12, 10));
 		j3.añadirCarta(new Carta(Palo.OROS, 12, 10));
 		j3.añadirCarta(new Carta(Palo.OROS, 7, 7));
 		//
@@ -56,16 +59,21 @@ public class TestGestorFasesLance {
 		j4.añadirCarta(new Carta(Palo.COPAS, 11, 10));
 		j4.añadirCarta(new Carta(Palo.COPAS, 12, 10));
 		j4.añadirCarta(new Carta(Palo.COPAS, 7, 7));
+	}
+
+	@Before
+	public void inicializar(){
+		p = new Partida();
 		p.sentarJugador(j1, 0);
 		p.sentarJugador(j2, 1);
 		p.sentarJugador(j3, 2);
 		p.sentarJugador(j4, 3);
 		p.empezarPartida();
-		
 		miGestor = new GestorFasesLance();
 		miGestor.setPartida(p);
 		miGestor.setParesJuegos(new ComprobadorParesJuego());
 	}
+	
 	/**
 	 * Recupera a quién le toca jugar a partir de una partida
 	 * iniciada.
@@ -120,6 +128,181 @@ public class TestGestorFasesLance {
 		miGestor.faseChica(j4, AccionesLance.PASO, 0);
 		Lances lance = miGestor.getFase();
 		Assert.assertEquals("El siguiente lance es :",
-				Lances.PARES, lance);
+				Lances.JUEGO, lance);
+	}
+	
+	@Test
+	public void testEnvidoNoQuiero() {
+		gestorApuestas = new GestorDeApuestas();
+		miGestor.setPartida(p);
+		int turno = miGestor.getTurno();
+		Jugador[] jugadores = p.getMesa();
+		miGestor.setGestorApuestas(gestorApuestas);
+		miGestor.faseGrande(jugadores[turno], AccionesLance.ENVIDO, 2);
+		int mano = turno;
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.NOQUIERO, 0);
+		turno = miGestor.getTurno();
+		Assert.assertEquals("El turno es del jugador :",
+				(mano+3)%4, turno);
+	}
+	
+	@Test
+	public void testEnvidoQuiero() {
+		gestorApuestas = new GestorDeApuestas();
+		miGestor.setPartida(p);
+		int turno = miGestor.getTurno();
+		Jugador[] jugadores = p.getMesa();
+		miGestor.setGestorApuestas(gestorApuestas);
+		miGestor.faseGrande(jugadores[turno], AccionesLance.ENVIDO, 2);
+		int mano = turno;
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.QUIERO, 0);
+		Lances lance = miGestor.getFase();
+		Assert.assertEquals("El siguiente lance es :",
+				Lances.CHICA, lance);
+	}
+	
+	@Test
+	public void testEnvidoNoQuieroNoQuiero() {
+		gestorApuestas = new GestorDeApuestas();
+		miGestor.setPartida(p);
+		int turno = miGestor.getTurno();
+		Jugador[] jugadores = p.getMesa();
+		miGestor.setGestorApuestas(gestorApuestas);
+		miGestor.faseGrande(jugadores[turno], AccionesLance.ENVIDO, 2);
+		int mano = turno;
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.NOQUIERO, 0);
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.NOQUIERO, 0);
+		Lances lance = miGestor.getFase();
+		Assert.assertEquals("El siguiente lance es :",
+				Lances.CHICA, lance);
+	}
+	
+	@Test
+	public void testEnvidoNoQuieroQuiero() {
+		gestorApuestas = new GestorDeApuestas();
+		miGestor.setPartida(p);
+		int turno = miGestor.getTurno();
+		Jugador[] jugadores = p.getMesa();
+		miGestor.setGestorApuestas(gestorApuestas);
+		miGestor.faseGrande(jugadores[turno], AccionesLance.ENVIDO, 2);
+		int mano = turno;
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.NOQUIERO, 0);
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.QUIERO, 0);
+		Lances lance = miGestor.getFase();
+		Assert.assertEquals("El siguiente lance es :",
+				Lances.CHICA, lance);
+	}
+	
+	@Test
+	public void testPasoPasoPasoEnvido() {
+		gestorApuestas = new GestorDeApuestas();
+		miGestor.setPartida(p);
+		int turno = miGestor.getTurno();
+		Jugador[] jugadores = p.getMesa();
+		miGestor.setGestorApuestas(gestorApuestas);
+		miGestor.faseGrande(jugadores[turno], AccionesLance.PASO, 0);
+		int mano = turno;
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.PASO, 0);
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.PASO, 0);
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.ENVIDO, 2);
+		turno = miGestor.getTurno();
+		Assert.assertEquals("El turno es del jugador :",
+				mano, turno);
+	}
+	
+	@Test
+	public void testEnvidoNoQuieroSubo() {
+		gestorApuestas = new GestorDeApuestas();
+		miGestor.setPartida(p);
+		int turno = miGestor.getTurno();
+		Jugador[] jugadores = p.getMesa();
+		miGestor.setGestorApuestas(gestorApuestas);
+		miGestor.faseGrande(jugadores[turno], AccionesLance.ENVIDO, 2);
+		int mano = turno;
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.NOQUIERO, 0);
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.APUESTA, 2);
+		turno = miGestor.getTurno();
+		Assert.assertEquals("El turno es del jugador :",
+				mano, turno);
+	}
+	
+	@Test
+	public void testPasoEnvidoSuboNoQuiero() {
+		gestorApuestas = new GestorDeApuestas();
+		miGestor.setPartida(p);
+		int turno = miGestor.getTurno();
+		Jugador[] jugadores = p.getMesa();
+		miGestor.setGestorApuestas(gestorApuestas);
+		miGestor.faseGrande(jugadores[turno], AccionesLance.PASO, 0);
+		int mano = turno;
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.ENVIDO, 2);
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.APUESTA, 5);
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.NOQUIERO, 0);
+		turno = miGestor.getTurno();
+		Assert.assertEquals("El turno es del jugador :",
+				((mano+1)%4), turno);
+	}
+	
+	@Test
+	public void testTurnoEnvidoSuboQuiero() {
+		gestorApuestas = new GestorDeApuestas();
+		miGestor.setPartida(p);
+		int turno = miGestor.getTurno();
+		Jugador[] jugadores = p.getMesa();
+		miGestor.setGestorApuestas(gestorApuestas);
+		miGestor.faseGrande(jugadores[turno], AccionesLance.ENVIDO, 2);
+		int mano = turno;
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.APUESTA, 5);
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.QUIERO, 0);
+		turno = miGestor.getTurno();
+		Assert.assertEquals("El turno es del jugador :",
+				mano, turno);
+	}
+	
+	@Test
+	public void testFaseEnvidoSuboQuiero() {
+		gestorApuestas = new GestorDeApuestas();
+		miGestor.setPartida(p);
+		int turno = miGestor.getTurno();
+		Jugador[] jugadores = p.getMesa();
+		miGestor.setGestorApuestas(gestorApuestas);
+		miGestor.faseGrande(jugadores[turno], AccionesLance.ENVIDO, 2);
+		int mano = turno;
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.APUESTA, 5);
+		turno = miGestor.getTurno();
+		miGestor.faseGrande(jugadores[turno], AccionesLance.QUIERO, 0);
+		Lances lance = miGestor.getFase();
+		Assert.assertEquals("El siguiente lance es :",
+				Lances.CHICA, lance);
+	}
+	
+	@Test
+	public void testMismoPasoEnvido() {
+		gestorApuestas = new GestorDeApuestas();
+		miGestor.setPartida(p);
+		int turno = miGestor.getTurno();
+		Jugador[] jugadores = p.getMesa();
+		miGestor.setGestorApuestas(gestorApuestas);
+		miGestor.faseGrande(jugadores[turno], AccionesLance.ENVIDO, 2);
+		boolean resultado = miGestor.faseGrande(jugadores[turno], AccionesLance.PASO, 0);
+		Assert.assertEquals("No se permite PASO al jugador que ya envidó",false, resultado);
+	
 	}
 }
